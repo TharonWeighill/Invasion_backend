39a8c691f7be45562c77ef015ee23f28a17a3dd7
tharon <tharonweighill@gmail.com>
2021-06-01 13:57:17 -0600

    create player table

--- db/migrate/20210601195549_create_player.rb
+++ db/migrate/20210601195549_create_player.rb
@@ -1,7 +1,8 @@
 class CreatePlayer < ActiveRecord::Migration[6.1]
   def change
     create_table :players do |t|
-
+      t.string :name
+      t.string :species
       t.timestamps
     end
   end
