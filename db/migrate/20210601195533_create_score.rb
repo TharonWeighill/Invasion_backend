448fa61011eb0fa66a14c0b440ab01ce8c8eee56
tharon <tharonweighill@gmail.com>
2021-06-01 13:58:07 -0600

    create score table

--- db/migrate/20210601195533_create_score.rb
+++ db/migrate/20210601195533_create_score.rb
@@ -1,7 +1,8 @@
 class CreateScore < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
-
+      t.integer :score
+      t.integer :destroyed
       t.timestamps
     end
   end
