fac8b46d88b29eaa86004650208306bff77f400f
tharon <tharonweighill@gmail.com>
2021-06-02 11:17:05 -0600

    migrate database

--- db/migrate/20210601195533_create_score.rb
+++ db/migrate/20210601195533_create_score.rb
@@ -3,6 +3,8 @@ class CreateScore < ActiveRecord::Migration[6.1]
     create_table :scores do |t|
       t.integer :score
       t.integer :destroyed
+      t.belongs_to :player, null: false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,11 @@ ActiveRecord::Schema.define(version: 2021_06_01_195549) do
   create_table "scores", force: :cascade do |t|
     t.integer "score"
     t.integer "destroyed"
+    t.integer "player_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_scores_on_player_id"
   end
 
+  add_foreign_key "scores", "players"
 end
